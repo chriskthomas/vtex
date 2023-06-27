5011dc51acbfab1349dfd8d9a9fed31ec7c56db4
Chris K. Thomas <135461249+chriskthomas@users.noreply.github.com>
2023-06-26 21:49:41 -0500

    adapt for me

--- generate.bash
+++ generate.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-TINYTEX_VERSION="2023.03"
+TINYTEX_VERSION="2023.06"
 TEXDIR="vtex"
 
 TINYTEX_URL="https://github.com/rstudio/tinytex-releases/releases/download/v$TINYTEX_VERSION/TinyTeX-0-v$TINYTEX_VERSION"
@@ -15,10 +15,7 @@ export PATH="$(pwd):$PATH"
 cd -
 
 # install requred
-tlmgr install latex-bin tools kvoptions etoolbox pdftexcmds infwarerr everysel parskip hyperref geometry sectsty ragged2e enumitem fontawesome markdown fontspec paralist amsmath amsfonts csvsimple fancyvrb pgf gobble epstopdf-pkg
-
-# generate font names database for lualatex (speedup generation later)
-luaotfload-tool --update && echo "LuaLaTeX font database generated (exit: $?)" || echo "LuaLaTeX font database generation failed (exit: $?)"
+tlmgr install latex-bin tools kvoptions etoolbox pdftexcmds infwarerr everysel parskip hyperref geometry sectsty ragged2e enumitem fontawesome xifthen ifmtarg
 
 # package into final archive
 tar -czf "vtex.tar.gz" "${TEXDIR}" && echo "vtex.tar.gz generated (exit: $?)" || echo "vtex.tar.gz generation failed (exit: $?)"
